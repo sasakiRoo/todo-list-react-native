5ead2a3e54097ddeae81213eeec5112bcdf5f217
sasakiroo <muhammadrobisasaki@gmail.com>
2022-07-14 11:30:16 +0800

    added some state

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
+import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Task'
 
 
 export default function App() {
+
+  const [task, setTask] = useState()
+  const [taskItems, setTaskItems] = useState([])
+
+  const handleAddTask = () => {
+    Keyboard.dismiss()
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const handleDeleteTask = (index) => {
+    let itemsCopy = [...taskItems]
+    itemsCopy.splice(index, 1)
+    setTaskItems(itemsCopy)
+  }
+
   return (
     <View style={styles.container}>
 
@@ -13,10 +30,31 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's tasks</Text>
 
         <View style={styles.items}>
-            <Task text={'hello gaes'}/>
-            <Task text={'hello gaes'}/>
+          <ScrollView>
+              {
+                taskItems.map((item, index) => {
+                  return (
+                    <TouchableOpacity key={index} onPress={() => handleDeleteTask(index)}>
+                      <Task text={item}/>
+                    </TouchableOpacity> 
+                  )
+                })
+              }
+          </ScrollView>
         </View>
       </View>
+
+    {/*write tasks*/}
+
+    <KeyboardAvoidingView style={styles.writeTask} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+      <TextInput style={styles.inputText} placeholder={'ketik disini!'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()} >
+          <View style={styles.btnAddWrapper}>
+            <Text style={styles.addTextBtn}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
 
     </View>
   );
@@ -37,5 +75,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 12,
+    height: '90%',
+  },
+  writeTask: {
+    position: 'absolute',
+    bottom: 50,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  inputText: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    width: 250,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+  },
+  btnAddWrapper: {
+    width: 45,
+    height: 45,
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  addTextBtn: {
+    fontSize: 25,
   },
 });
